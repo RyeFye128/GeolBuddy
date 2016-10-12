7553292c6a2b0b7b85496d25db35cf7b2cd24d37
Ryan Chartier <ryefye@outlook.com>
2016-10-11 23:53:35 -0600

    Filled out Home

---
+++ Geolbuddy/app/src/main/java/com/example/ryanspc/geolbuddy/Home.java
@@ -0,0 +1,52 @@
+package com.example.ryanspc.geolbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Home extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView headerImage;
+    Button rockIdentification, rockLookup, mineralLookup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        headerImage = new ImageView(this);
+
+        rockIdentification = (Button) findViewById(R.id.rockIdent);
+        rockLookup = (Button)findViewById(R.id.rockLookup);
+        mineralLookup = (Button) findViewById(R.id.mineralLookup);
+
+        rockIdentification.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //decide action based on button id
+        Button temp = (Button)v;
+        switch(temp.getText().toString())
+        {
+            case "Rock Identification":
+                //stuff
+                //Intent intent = new Intent(Home.this, choose_flow_chart.class);
+                //startActivity(intent);
+
+                break;
+            case "Rock Lookup":
+                //Premium
+                break;
+            case "Mineral Lookup":
+                //Premium
+                break;
+        }
+    }
+}
